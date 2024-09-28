605ec36c32846e316630e4c600244d4a7c697711
Riccardo Casatta <riccardo@casatta.it>
2024-09-28 22:58:54 +0200

    add summars

--- flake.nix
+++ flake.nix
@@ -58,39 +58,62 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+    summars = {
+      url = "github:daywalker90/summars";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
 
   };
 
-  outputs = { self, nixpkgs, flake-utils, blocks_iterator, fbbe, waterfalls, eternitywall, opreturn_org, lightdash, pay2email, electrs, esplora-enterprise-monitoring, post2fs, sling }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , blocks_iterator
+    , fbbe
+    , waterfalls
+    , eternitywall
+    , opreturn_org
+    , lightdash
+    , pay2email
+    , electrs
+    , esplora-enterprise-monitoring
+    , post2fs
+    , sling
+    , summars
+    }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        blocks_iterator_pkg = blocks_iterator.packages.${system};
-        fbbe_pkg = fbbe.packages.${system};
-        waterfalls_pkg = waterfalls.packages.${system};
-        eternitywall_pkg = eternitywall.packages.${system};
-        opreturn_org_pkg = opreturn_org.packages.${system};
-        lightdash_pkg = lightdash.packages.${system};
-        pay2email_pkg = pay2email.packages.${system};
-        electrs_pkg = electrs.packages.${system};
-        esplora-enterprise-monitoring_pkg = esplora-enterprise-monitoring.packages.${system};
-        post2fs_pkg = post2fs.packages.${system};
-        sling_pkg = sling.packages.${system};
+    let
+      blocks_iterator_pkg = blocks_iterator.packages.${system};
+      fbbe_pkg = fbbe.packages.${system};
+      waterfalls_pkg = waterfalls.packages.${system};
+      eternitywall_pkg = eternitywall.packages.${system};
+      opreturn_org_pkg = opreturn_org.packages.${system};
+      lightdash_pkg = lightdash.packages.${system};
+      pay2email_pkg = pay2email.packages.${system};
+      electrs_pkg = electrs.packages.${system};
+      esplora-enterprise-monitoring_pkg = esplora-enterprise-monitoring.packages.${system};
+      post2fs_pkg = post2fs.packages.${system};
+      sling_pkg = sling.packages.${system};
+      summars_pkg = summars.packages.${system};
 
 
-      in
-      {
-        packages.blocks_iterator = blocks_iterator_pkg.default;
-        packages.fbbe = fbbe_pkg.default;
-        packages.waterfalls = waterfalls_pkg.default;
-        packages.eternitywall = eternitywall_pkg.default;
-        packages.opreturn_org = opreturn_org_pkg.default;
-        packages.lightdash = lightdash_pkg.default;
-        packages.pay2email = pay2email_pkg.default;
-        packages.electrs = electrs_pkg.bin;
-        packages.electrs_liquid = electrs_pkg.binLiquid;
-        packages.esplora-enterprise-monitoring = esplora-enterprise-monitoring_pkg.default;
-        packages.post2fs = post2fs_pkg.default;
-        packages.sling = sling_pkg.default;
+    in
+    {
+      packages.blocks_iterator = blocks_iterator_pkg.default;
+      packages.fbbe = fbbe_pkg.default;
+      packages.waterfalls = waterfalls_pkg.default;
+      packages.eternitywall = eternitywall_pkg.default;
+      packages.opreturn_org = opreturn_org_pkg.default;
+      packages.lightdash = lightdash_pkg.default;
+      packages.pay2email = pay2email_pkg.default;
+      packages.electrs = electrs_pkg.bin;
+      packages.electrs_liquid = electrs_pkg.binLiquid;
+      packages.esplora-enterprise-monitoring = esplora-enterprise-monitoring_pkg.default;
+      packages.post2fs = post2fs_pkg.default;
+      packages.sling = sling_pkg.default;
+      packages.summars = summars_pkg.default;
 
-      });
+    });
 }
