7dafad3e3099661aafe2f195ca1c44460f2da8d1
Riccardo Casatta <riccardo@casatta.it>
2025-04-06 23:09:16 +0200

    revert waterfalls to master

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     waterfalls = {
-      url = "github:RCasatta/waterfalls/fix_history_updates";
+      url = "github:RCasatta/waterfalls";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
