280e7ac9c57172ac84d44322ffaceadba98e8651
Riccardo Casatta <riccardo@casatta.it>
2025-08-14 08:28:03 +0200

    update

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     waterfalls_bitcoin = {
-      url = "github:RCasatta/waterfalls/reorg";
+      url = "github:RCasatta/waterfalls/reorg3";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
