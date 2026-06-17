c7f0316a87a0b454a90f07e3278d9fe0de362a6e
Riccardo Casatta <riccardo@casatta.it>
2026-06-17 10:03:12 +0200

    u

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     waterfalls = {
-      url = "github:RCasatta/waterfalls/compute_max";
+      url = "github:RCasatta/waterfalls";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
